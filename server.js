67af0db75c6bd5266caa8eda8dd781dfc60393b8
AA McCANN laptop <ameen.aziz@mccann.com>
2019-05-17 10:48:01 +0100

    temaplating using json data and saving to filesystem functional

---
+++ server.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const express = require('express'); 
+const Handlebars = require('handlebars');
+const app = express();
+app.set('port', (process.env.PORT || 3010));
+
+const source = fs.readFileSync('template.html', 'utf8');
+const template = Handlebars.compile(source);
+   
+const email_dataRaw = fs.readFileSync('data.json', 'utf8');
+const email_dataParsed = JSON.parse(email_dataRaw); 
+
+const result = template(email_dataParsed);  
+
+app.get('/', function(req, res) {
+    res.send(result);
+});
+
+app.use('/html', function(req, res) {
+  res.render('index.html', result);
+})
+
+const stream = fs.createWriteStream("./dist/index.html");
+stream.once('open', function(fd) {
+  stream.write(result);
+  stream.end();
+});
+
+
+app.listen(app.get('port'), function(){
+    console.log('Server started on port ' + app.get('port'));
+} )
+
+
+
