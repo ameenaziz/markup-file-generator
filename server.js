fb1d5d0db8e78e4a742c3df47a1c5d56cf81a58e
Ameen Aziz <ameenaziz@RBTSecAdmins-MacBook-Pro.local>
2022-02-08 20:40:04 +0000

    add link to start console message

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ stream.once('open', () => {
 
 
 app.listen(app.get('port'), function(){
-    console.log('Server started on port ' + app.get('port'));
+    console.log('Server started on port http://localhost:' + app.get('port'));
 } )
 
 
